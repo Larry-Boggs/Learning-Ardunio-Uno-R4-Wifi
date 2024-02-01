80208a0841cccac0583deb2a99b286eb9fb81c5d
Larry-Boggs <158508292+Larry-Boggs@users.noreply.github.com>
2024-02-01 13:34:01 -0500

    Add files via upload

---
+++ SOS_Homework.ino
@@ -0,0 +1,46 @@
+void setup() {
+ pinMode(13,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(200);
+  digitalWrite(13,LOW);
+  delay(200);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  delay(500);
+}
